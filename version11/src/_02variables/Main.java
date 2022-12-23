5bd87239d639ab7cdd807aa37de4e41e18d2eb17
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2022-12-22 23:26:15 -0300

    feat: refactoring variables

--- version11/src/_02variables/Main.java
+++ version11/src/_02variables/Main.java
@@ -2,19 +2,43 @@ package _02variables;
 
 public class Main {
   public static void main(String[] args) {
+    variables();
+  }
+
+  static void variables() {
+    defaultInitialized();
+
+    defaultNotInitialized();
+
+    inferredType();
+
+    constant();
+
+    multiplyInitialization();
+  }
+
+  static void defaultInitialized() {
     // TIPO NOME = VALOR;
     String name = "Luiz";
+  }
 
-    // var NOME = VALOR;
-    var name2 = "Luiz";
-
+  static void defaultNotInitialized() {
     // TIPO NOME; => recebe o valor ZERO do tipo
     String nome3;
     nome3 = "Luiz";
+  }
 
+  static void inferredType() {
+    // var NOME = VALOR;
+    var name2 = "Luiz";
+  }
+
+  static void constant() {
     // final TIPO NOME = VALOR;
     final String name4 = "Luiz";
+  }
 
+  static void multiplyInitialization() {
     // TIPO NOME, NOME1, NOME2;
     String a, b, c;
   }
