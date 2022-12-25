99a02ecc7454c09a64d860dfc318657b34a0b0a0
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2022-12-25 00:39:12 -0300

    feat: add operators

--- version11/src/_04operators/Main.java
+++ version11/src/_04operators/Main.java
@@ -7,17 +7,17 @@ public class Main {
   }
 
   static void operators() {
-//    arithmetics();
-//
-//    assignment();
-//
-//    relational();
-//
-//    logical();
-//
-//    unary();
-//
-//    ternary();
+    arithmetics();
+
+    assignment();
+
+    relational();
+
+    logical();
+
+    unary();
+
+    ternary();
 
     bitwise();
   }
